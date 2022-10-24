335d672de96f855d5755df6c3970323725442454
master <master.code.develop@gmail.com>
2022-10-24 17:12:40 +0200

    ref photographer article (use template)

--- scripts/layouts/photographerArticle.js
+++ scripts/layouts/photographerArticle.js
@@ -4,55 +4,42 @@
  * @returns article element
  */
 export function photographerArticle(data) {
-    /*===== data for one photographer =====*/
-    const { id, name, portrait, city, country, tagline, price } = data;
+    /*========== CONST ==========*/
+    const { id, name, portrait, city, country, tagline, price } = data,
+    templateArticle = document.getElementById('photographer-article').content.cloneNode(true),
+    article = document.createElement('article'),
+    imgLink =`./assets/photographers/${portrait}/`;
 
-    /*===== Create all element of photographer card  =====*/
-    // Article
-    const article = document.createElement('article');
-    article.className = 'photographer-article';
-    
-    // link
-    const a = document.createElement('a');
+    /*========== CLONE PHOTOGRAPHER CARD FROM TEMPLATE HTML  ==========*/
+    article.className = 'photographer-article'; 
+    article.append(templateArticle);
+
+    /*========== CONST OF ARTICLE  ==========*/
+    const a = article.querySelector('a'),
+    sources = article.querySelectorAll('source'),
+    img = article.querySelector('img'),
+    h2 = article.querySelector('h2'),
+    h3 = article.querySelector('h3'),
+    spans = article.querySelectorAll('span');
+
+    /*========== UPDATE ARTICLE  ==========*/
+    /*## LINK ##*/
     a.href = `./photographer.html?id=${id}`;
-
-    // image
-    const img = document.createElement( 'img' );
-    img.src = `assets/photographers/${portrait}`;
-    img.className = 'photographer-article__picture';
-    a.alt = name;
-
-    // title
-    const h2 = document.createElement('h2');
-    h2.className = 'photographer-article__name';
+    /*## PICTURE ##*/
+    sources[0].srcset = imgLink+"s.jpg";
+    sources[1].srcset = imgLink+"m.jpg";
+    sources[2].srcset = imgLink+"l.jpg";
+    img.src = imgLink+"xl.jpg";
+    img.alt = name;
+    /*## TITLE ##*/
     h2.ariaLabel = name;
     h2.textContent = name;
+    /*## LOCATION ##*/
+    h3.textContent = name;
+    /*## TAGLINE ##*/
+    spans[1].textContent = tagline;
+    /*## PRICE ##*/
+    spans[1].textContent = price + "€/jour";
 
-    // City, Country
-    const locationHeading = document.createElement('h3');
-    locationHeading.textContent = city + ', ' + country;
-    locationHeading.classList.add("photographer-article__location")
-
-    // Tagline
-    const taglineSpan = document.createElement('span');
-    taglineSpan.textContent = tagline;
-    taglineSpan.classList.add("photographer-article__tagline");
-
-    // Price
-    const priceSpan = document.createElement( 'span' );
-    priceSpan.textContent = price + "€/jour";
-    priceSpan.classList.add("photographer-article__price");
-    
-    /*===== Append all element to link then to article =====*/
-    // apend to link
-    a.appendChild(img);
-    a.appendChild(h2);
-
-    // append to the article
-    article.appendChild(a);
-    article.appendChild(locationHeading);
-    article.appendChild(taglineSpan);
-    article.appendChild(priceSpan);
-    
     return (article);
 }
