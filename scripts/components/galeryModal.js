9b1603a480d120d72522351b36e20d4421685f7b
master <master.code.develop@gmail.com>
2022-10-29 17:43:17 +0200

    ref Modal Galery responsive(css) arria html

--- scripts/components/galeryModal.js
+++ scripts/components/galeryModal.js
@@ -1,16 +1,17 @@
 /**########################### CONST ###########################**/
-const cards = document.getElementsByClassName('card'),
+const body = document.querySelector('body'),
+photographer = document.getElementById('photographer'),
+cards = document.getElementsByClassName('card'),
 modal = document.getElementById('modal-galery'),
 arrows = modal.querySelectorAll('a'),
 figure =  modal.querySelector('figure'),
 figcaption = document.createElement('figcaption'),
 btn = modal.querySelector('button');
 
+/**########################### VARIABLES ###########################**/
+var index, data;
 
 /**########################### FUNCTIONS ###########################**/
-function closeModal() {
-    modal.style.display = 'none';
-}
 function updateGalery(data) {
     const { photographerId, image, video, title } = data;
     figure.innerHTML='';
@@ -24,17 +25,90 @@ function updateGalery(data) {
     /*========== VIDEO ==========*/
     else if (video) {
         const newVideo = document.createElement('video');
-        newVideo.className = 'card-video1';
-        newVideo.controls=true;
+        newVideo.controls = true;
         newVideo.src = `./assets/photos/${photographerId}/${video}`;
         figure.appendChild(newVideo);
     }
-
+    
+    /*========== FIGCAPTION ==========*/
     figcaption.textContent = title;
     figcaption.className = 'modal-galery__title';
     figcaption.tabIndex = 0;
     figure.appendChild(figcaption);
 
+    /*========== ARROWS ==========*/
+    arrows[0].addEventListener('click', previousElement);// left-arrow
+    arrows[1].addEventListener('click', nextElement);// right-arrow
+
+    /*========== ARRIA ==========*/
+    photographer.ariaHidden = true;
+    modal.ariaHidden = false;
+    modal.focus();
+
+    body.classList.add('no-scroll'); // hidden the body scroll
+    photographer.classList.add('no-scroll'); // hidden the body scroll
+    modal.classList.add('is-open');// display the modal
+    window.addEventListener('keydown', handleKeydown, false);// keydown control
+}
+/**
+ * change to previous photo/videochange to previous photo/video
+ */
+function previousElement() {
+    index = (index==0)? data.length-1 : index-1;
+    updateGalery(data[index]);
+}
+/**
+ * change to next photo/video
+ */
+function nextElement() {
+    index = (index==(data.length-1))? 0 : index+1;
+    updateGalery(data[index]);
+}
+/**
+ * Close the modal and focus the last show element (photo/video)
+ */
+function closeModal() {
+    const cardImg = (data[index].image) 
+    ?   cards[index].querySelector('img')
+    :   cards[index].querySelector('video');
+
+    /*========== ARRIA ==========*/
+    photographer.ariaHidden = false;
+    modal.ariaHidden = true;
+    cardImg.focus();
+
+    /*========== REMOVE ==========*/
+    body.classList.remove('no-scroll');
+    photographer.classList.remove('no-scroll');
+    modal.classList.remove('is-open');
+    window.removeEventListener('keydown', handleKeydown, false);
+}
+/**
+ * Controls keyboard navigation of the ArrowLeft, ArrowRight, Escape, Enter and Space for the modal galery.
+ * @param {event} e event handling.
+ */
+function handleKeydown (e) {
+    const keyCode = e.keyCode ? e.keyCode : e.which,
+    activeElement = document.activeElement,
+    video = modal.querySelector('video');
+    //e.preventDefault();
+    //document.activeElement = modal;
+    if(keyCode === 37) previousElement();//ArrowLeft
+    else if(keyCode === 39) nextElement();//ArrowRight
+    else if(keyCode === 27) closeModal(); // Escape
+    else if (keyCode === 32) {
+        if(video) (video.paused)   
+        ?   video.play()
+        :   video.pause();
+    }/* Space */
+  
+    // control Tab
+    const a = ( keyCode === 9 && activeElement == btn ),
+    b = ((e.shiftKey && keyCode === 9) && activeElement == arrows[0]),
+    c = (activeElement.parentElement != modal && activeElement.parentElement.parentElement != modal);
+    if( a || b || c ) {
+        modal.focus();
+    }
 }
 
 /**########################### EVENT LISTENER ###########################**/
@@ -44,49 +118,11 @@ btn.addEventListener('keydown', e => {
 })
 
 /**########################### EXPORT ###########################**/
-export function show(galeryID, data) {
-    var index = data.findIndex(e => e.id === galeryID);
-    //=== change to previous photo/video ===//
-    const previousElement = () => {
-        index = (index==0)
-        ?   data.length-1
-        : index-1;
-        updateGalery(data[index]);
-    },
-    //=== change to next photo/video ===//
-    nextElement = () => {
-        index = (index==(data.length-1))
-        ?   0
-        : index+1;
-        updateGalery(data[index]);
-    },
-    close = () => {
-        const cardImg = (data[index].image) 
-        ?   cards[index].querySelector('img')
-        :   cards[index].querySelector('video');
-        
-        cardImg.focus();
-        window.removeEventListener('keydown', handleKeydown, true)
-        closeModal();
-    },
-    handleKeydown = (e) => {
-        e.preventDefault();
-
-        if(e.key === 'ArrowLeft') previousElement();
-        else if(e.key === 'ArrowRight') nextElement();
-        else if(e.key === 'Escape') close();
-        else if (e.key === ' ') {
-            const video = modal.querySelector('video');
-            if(video) (video.paused)   
-            ?   video.play()
-            :   video.pause();
-            
-        }
-    }
-    updateGalery(data[index])
-    arrows[0].addEventListener('click', previousElement)
-    arrows[1].addEventListener('click', nextElement)
-    btn.addEventListener('click', close)
-    modal.style.display= 'flex';
-    window.addEventListener('keydown', handleKeydown, true);
+export function show(galeryID, DATA) {
+    // update variables
+    index = DATA.findIndex(e => e.id === galeryID);
+    data = DATA;
+    
+    // update the galery sections
+    updateGalery(data[index]);
 }
